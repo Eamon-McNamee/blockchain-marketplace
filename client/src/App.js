ac3c2be3ff9a373f8264025f9722c1b742ec5fca
Eamon-McNamee <eamonmcnamee@gmail.com>
2024-06-20 17:40:01 +0100

    use axios to get items from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchItems } from './services/apiService';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems()
+      .then(response => setItems(response.data.items))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
       <h1>Blockchain Marketplace</h1>
+      <div>
+        <h1>Items</h1>
+        <ul className="item-list">
+          {items.map((item, index) => (
+            <li key={index}>
+              <strong>{item.name}</strong>: ${item.price.toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
